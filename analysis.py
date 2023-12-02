4ceaed11fe85e1694626c9523d7b7eb3525c7a87
UbahrMayInsch <chinmaysharma.dps@gmail.com>
2023-12-02 06:57:40 +0530

    changed analysis

--- analysis.py
+++ analysis.py
@@ -3,30 +3,30 @@ import colours
 import subprocess as sp
 from datetime import datetime
 
-def a():
-    todayDate = datetime.today().strftime('%Y-%m-%d')
-    getDate = input("Enter date from when you want in form yyyy-mm-dd: ")
-    query = f"""
-SELECT Region_ID, SUM(Packages) / DATEDIFF('{todayDate}', '{getDate}') as Pac
-FROM
-(
-    SELECT SUM(Num_Pkg_bought) as Packages, Falc_Emp_ID as FID
-    FROM Buys
-    WHERE Trans_Date >= '{getDate}'
-    GROUP BY FID
-) as A
-INNER JOIN
-(
-    SELECT Employee_ID, Region_ID
-    FROM Employee
-    WHERE Employee_Type = 'Falcon'
-) as F
-ON F.Employee_ID = A.FID
-GROUP BY F.Region_ID;
-    """
-    MiniWorld.executeQuery(query)
+# def a():
+#     todayDate = datetime.today().strftime('%Y-%m-%d')
+#     getDate = input("Enter date from when you want in form yyyy-mm-dd: ")
+#     query = f"""
+# SELECT Region_ID, SUM(Packages) / DATEDIFF('{todayDate}', '{getDate}') as Pac
+# FROM
+# (
+#     SELECT SUM(Num_Pkg_bought) as Packages, Falc_Emp_ID as FID
+#     FROM Buys
+#     WHERE Trans_Date >= '{getDate}'
+#     GROUP BY FID
+# ) as A
+# INNER JOIN
+# (
+#     SELECT Employee_ID, Region_ID
+#     FROM Employee
+#     WHERE Employee_Type = 'Falcon'
+# ) as F
+# ON F.Employee_ID = A.FID
+# GROUP BY F.Region_ID;
+#     """
+#     MiniWorld.executeQuery(query)
 
-def a1():
+def a():
     studentExam= input("Enter Exam")
     studentMarks= input("Enter Marks")
     query = f"""
@@ -39,7 +39,7 @@ def a1():
     """
     MiniWorld.executeQuery(query)
 
-def b1():
+def b():
     query=""" 
     SELECT PercentageOfPlacements
     FROM Colleges
@@ -51,7 +51,7 @@ def b1():
     """
     MiniWorld.executeQuery(query)
 
-def c1():
+def c():
     query="""
     SELECT IndustryField
     FROM Startups JOIN Colleges ON Startups.CollegeID = Colleges.CollegeID
@@ -59,49 +59,49 @@ def c1():
     """
     MiniWorld.executeQuery(query)
 
-def b():
-    startDate = input("Enter start date of the week in form yyyy-mm-dd: ")
-    query = f"""
-SELECT Ltnt_Emp_ID, Sum(amount) as Profit
-FROM
-(
-    SELECT FID, Packages * (Total_Pkg_buy - Total_Pkg_sell) as amount
-    FROM
-    (
-            SELECT Drug_ID as DID, sum(Num_Pkg_bought) as Packages, Falc_Emp_ID as FID
-            FROM Buys
-            WHERE Trans_Date >= '{startDate}' AND Trans_Date <= DATE_ADD('{startDate}', INTERVAL 7 DAY)
-            GROUP BY DID, Falc_Emp_ID
-    ) as F
-    INNER JOIN Drug
-    ON Drug_ID = DID
-) as A
-INNER JOIN Falcon
-ON Employee_ID = FID
-GROUP BY Ltnt_Emp_ID;
-"""
-    MiniWorld.executeQuery(query)
+# def b():
+#     startDate = input("Enter start date of the week in form yyyy-mm-dd: ")
+#     query = f"""
+# SELECT Ltnt_Emp_ID, Sum(amount) as Profit
+# FROM
+# (
+#     SELECT FID, Packages * (Total_Pkg_buy - Total_Pkg_sell) as amount
+#     FROM
+#     (
+#             SELECT Drug_ID as DID, sum(Num_Pkg_bought) as Packages, Falc_Emp_ID as FID
+#             FROM Buys
+#             WHERE Trans_Date >= '{startDate}' AND Trans_Date <= DATE_ADD('{startDate}', INTERVAL 7 DAY)
+#             GROUP BY DID, Falc_Emp_ID
+#     ) as F
+#     INNER JOIN Drug
+#     ON Drug_ID = DID
+# ) as A
+# INNER JOIN Falcon
+# ON Employee_ID = FID
+# GROUP BY Ltnt_Emp_ID;
+# """
+#     MiniWorld.executeQuery(query)
 
 
-def c():
-    query = """
-SELECT
-    Region_ID,
-    COUNT(Customer_ID) as Customers,
-    EXTRACT(MONTH FROM Trans_Date) as M,
-    EXTRACT(YEAR FROM Trans_Date) as Y
-FROM Buys
-INNER JOIN
-(
-    SELECT Employee_ID, Region_ID
-    FROM Employee
-    WHERE Employee_Type = 'Falcon'
-) as F
-ON Falc_Emp_ID  = Employee_ID
-GROUP BY Y, M, Region_ID
-ORDER BY Region_ID, Y, M ASC;
-    """
-    MiniWorld.executeQuery(query)
+# def c():
+#     query = """
+# SELECT
+#     Region_ID,
+#     COUNT(Customer_ID) as Customers,
+#     EXTRACT(MONTH FROM Trans_Date) as M,
+#     EXTRACT(YEAR FROM Trans_Date) as Y
+# FROM Buys
+# INNER JOIN
+# (
+#     SELECT Employee_ID, Region_ID
+#     FROM Employee
+#     WHERE Employee_Type = 'Falcon'
+# ) as F
+# ON Falc_Emp_ID  = Employee_ID
+# GROUP BY Y, M, Region_ID
+# ORDER BY Region_ID, Y, M ASC;
+#     """
+#     MiniWorld.executeQuery(query)
 
 
 def analysis():
@@ -109,9 +109,9 @@ def analysis():
         tmp = sp.call('clear', shell=True)
         print("Choose an operation:")
         print(f"{colours.bcolors.OKCYAN}")
-        print("1. Average number of packages sold in a region after a particular date")
-        print("2. Profit a lieutenant generates in a week")
-        print("3. Change in number of buyers in a territory over the months")
+        print("1. Find a list of branches you are eligible for based on a score in an exam")
+        print("2. Percentage of placements in the college with the highest NIRF ranking")
+        print("3. Industries in which startups founded in Mumbai Colleges are working")
         print(f"{colours.bcolors.ENDC}{colours.bcolors.WARNING}")
         print("4. Back")
         print("5. Exit")
@@ -120,11 +120,11 @@ def analysis():
         ch = input("Enter choice: ").lower()
         tmp = sp.call('clear', shell=True)
 
-        if ch == '1' or ch == 'average number of packages sold in a region after a particular date':
+        if ch == '1' or ch == 'Find a list of branches you are eligible for based on a score in an exam':
             a()
-        elif ch == '2' or ch == 'profit a lieutenant generates in a week':
+        elif ch == '2' or ch == 'Percentage of placements in the college with the highest NIRF ranking':
             b()
-        elif ch == '3' or ch == 'change of number of buyers in a territory over the months':
+        elif ch == '3' or ch == 'Industries in which startups founded in Mumbai Colleges are working':
             c()
         elif ch == '4' or ch == 'back':
             return
